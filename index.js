8c4633f3dc50e0bfa6a85f11b676f3fdc2ae07e3
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-11-09 14:30:39 -0600

    commit tree fe455486de82ecdfdf26124efd99a0aed1e76a70

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+
+const findMatching = (string) => {
+    results = drivers.filter(filterByName(string))
+    return results
+}
+
+
+function filterByName(string) {
+    if (drivers.name === string){
+        return true;
+    }else {return false}
+}    
+
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+    
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
